e958a2dfacb0af4318ddd03ddf8125785cb51a04
Jacob Smith <jacob@jacobsmith.io>
2020-11-13 22:10:00 -0500

    Configure multiline logs

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -13,12 +13,17 @@ echo "
 logs_enabled: true
 " >> "$DD_CONF_DIR/datadog.yaml"
 
-mkdir "$DD_CONF_DIR/conf.d/$DEPLOY.d"
+mkdir -p "$DD_CONF_DIR/conf.d/$DEPLOY.d"
 
+RFC3339='([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))'
 echo "
 logs:
   - type: tcp
     port: 10518
     service: $DEPLOY
-    source: custom
+    source: heroku
+    log_processing_rules:
+      - type: multi_line
+        name: new_log_start_with_date
+        pattern: $RFC3339
 " >> "$DD_CONF_DIR/conf.d/$DEPLOY.d/conf.yaml"
